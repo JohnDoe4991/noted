0493675553d3ed6b292b83d2960e2d60e36bbdf7
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-06-15 23:42:49 +0200

    buttons

---
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -0,0 +1,29 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+    public function with(): array
+    {
+        return [
+            'notes' => Auth::user()->notes()->orderBy('send_date', 'asc')->get(),
+        ];
+    }
+}; ?>
+
+<div>
+    <div class="space-y-2">
+        <div class="grid-cols-2 gap-4 mt-12">
+            @foreach ($notes as $note)
+                <x-card wire:key='{{ $note->id }}'>
+                    <div class="flex justify-between">
+                        <a href='#'
+                            class="text-xl font-bold hover:underline hover:text-blue-500">{{ $note->title }}</a>
+                        <div class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
+                        </div>
+                    </div>
+                </x-card>
+            @endforeach
+        </div>
+    </div>
+</div>
