47934b29e4528c8e35c58b52be619e41e55826e2
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-06-16 00:36:29 +0200

    notes layout

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -22,6 +22,27 @@ new class extends Component {
                         <div class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
                         </div>
                     </div>
+                    <div class="flex items-end justify-between mt-4 space-x-1">
+                        <p class="text-xs"> Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
+                        <div>
+                            <button class="btn btn-circle btn-primary" onclick="viewNote()">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M19 8v8a2 2 0 01-2 2H7a2 2 0 01-2-2V8m3-5h8m-4 5v3" />
+                                </svg>
+                            </button>
+                            <button class="btn btn-circle btn-danger" onclick="deleteNote()">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+
                 </x-card>
             @endforeach
         </div>
